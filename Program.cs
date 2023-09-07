4780a70e92f5528ea96118319df574a95101a30c
mikeseal <mike@sealpoint.net>
2023-09-07 22:59:32 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Text.Json;
+
+#nullable enable
+
+// Define a class that matches the structure of your JSON data
+public class MyData
+{
+    public string? Name { get; set; }
+    public int Age { get; set; }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Specify the path to your JSON file
+        string jsonFilePath = "file.json";
+        List<MyData> myData = new List<MyData>();
+
+        try
+        {
+            IEnumerable<string> jsonLines = File.ReadLines(jsonFilePath);
+
+            foreach (string jsonLine in jsonLines)
+            {
+                MyData? myObject = JsonSerializer.Deserialize<MyData>(jsonLine);
+                if (myObject != null)
+                    myData.Add(myObject);
+            }
+
+            myData.ForEach(data =>
+            {
+                Console.WriteLine($"Name: {data.Name} Age: {data.Age}");
+            });
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"An error occurred: {ex.Message}");
+        }
+    }
+}
